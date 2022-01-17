7864be096d36d2a86e40cdb863efede1ed191e99
Taygeta <alvaromomblan@gmail.com>
2022-01-18 00:21:35 +0100

    implementados botones & actualizada logica de juego

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 const POSSIBLE_PLAYS = ['rock', 'paper', 'scissors'];
 function computerPlay() {
     return POSSIBLE_PLAYS[Math.floor(Math.random() * POSSIBLE_PLAYS.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (!POSSIBLE_PLAYS.includes(playerSelection.toLowerCase()) || !POSSIBLE_PLAYS.includes(computerSelection)) return -1;
+function playRound() {
+    const computerSelection = computerPlay();
+    const playerSelection = this.textContent;
+    let result;
     switch (playerSelection.toLowerCase()) {
         case 'rock':
-            return computerSelection === 'scissors' ? 1 : computerSelection === 'paper' ? 2 : 0;
+            result = computerSelection === 'scissors' ? 1 : computerSelection === 'paper' ? 2 : 0;
+            break;
         case 'paper':
-            return computerSelection === 'rock' ? 1 : computerSelection === 'scissors' ? 2 : 0;
+            result = computerSelection === 'rock' ? 1 : computerSelection === 'scissors' ? 2 : 0;
+            break;
         case 'scissors':
-            return computerSelection === 'paper' ? 1 : computerSelection === 'rock' ? 2 : 0;
+            result = computerSelection === 'paper' ? 1 : computerSelection === 'rock' ? 2 : 0;
+            break;
     }
+    if (result === 1) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        playerScore++;
+    } else if (result === 2) {
+        console.log(`You lose! ${playerSelection} loses to ${computerSelection}`);
+        computerScore++;
+    } else console.log('Thats a draw!')
+    round++;
+    Infogame();
+    if (playerScore === 5 || computerScore === 5) endGame(); 
 }
 
-function game() {
-    let round = 0;
-    let playerScore = 0;
-    let computerScore = 0;
-    console.log('Welcome to the game of rock, paper, scissors\n\n');
-    while (round < 5) {
-        console.log('\n---------------------')
-        console.log(`\nCurrent round: ${round}`)
-        console.log(`\nCurernt score --> Player: ${playerScore} --- Computer: ${computerScore}`);
-        const computerSelection = computerPlay();
-        const playerSelection = window.prompt('Select your play: ');
-        let result = playRound(playerSelection, computerSelection);
-        if (result === -1) console.log('Select a valid option (rock, paper or scissors)');
-        else {
-            if (result === 1) {
-                console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-                playerScore++;
-            } else if (result === 2) {
-                console.log(`You lose! ${playerSelection} loses to ${computerSelection}`);
-                computerScore++;
-            } else console.log('Thats a draw!')
-            round++;
-        }
-    }
-    console.log(playerScore > computerScore ? 'You win the game!' : 
-        playerScore < computerScore ? 'You lose the game...' : 'The game ended in a draw');
-    console.log(`\nFinal score --> Player: ${playerScore} --- Computer: ${computerScore}`);
+function Infogame() {
+    if (round === 0) console.log('Welcome to the game of rock, paper, scissors\n\n');
+    console.log('\n---------------------');
+    console.log(`\nCurrent round: ${round}`)
+    console.log(`\nCurernt score --> Player: ${playerScore} --- Computer: ${computerScore}`);
 }
 
-game();
+function endGame() {
+    if (playerScore === 5) console.log('You won the game!');
+    else console.log('You lose...');
+    playButtons.forEach(button => button.removeEventListener('click', playRound));
+    console.log('\n---------------------');
+    console.log('If you want to play again, please refresh the page');
+}
+
+const playButtons = document.querySelectorAll('button');
+playButtons.forEach(button => button.addEventListener('click', playRound));
+
+Infogame();
